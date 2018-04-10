21d92aa4f73d11e243c006621da5e31cc62e95b0
zouyubin <zouyubin@cloudsplus.com.cn>
2018-04-10 23:54:36 +0800

    程序源代码

---
+++ IOS开发从入门到精通/第一章/MyFirstIOSApp/MyFirstIOSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstIOSApp
+//
+//  Created by zouyb on 2018/4/10.
+//  Copyright © 2018年 zouyb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
